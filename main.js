b8886b7becb6e1dadc24341a35e5288ae442d426
Rafael-Camil0 <149325807+Rafael-Camil0@users.noreply.github.com>
2024-06-26 14:57:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const cookieImg = document.querySelector('#cookie');
+const again = document.querySelector('#again')
+const screen1 = document.querySelector('.screen')
+const screen2 = document.querySelector('.screen2')
+const txt = document.querySelector('#text')
+const thought = ["Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.","A felicidade depende de nós mesmos.","A coragem é a primeira das qualidades humanas porque garante todas as outras.","A excelência não é um ato, mas um hábito."]
+
+cookieImg.addEventListener("click",broken)
+
+again.addEventListener('click',(e)=>{
+    toogle()
+    
+}
+  
+)
+
+function broken() {
+    cookieImg.style.animation = "blurry 0.1s infinite"
+    getThought()
+    setTimeout(function(){
+        toogle()
+    }, 500);
+}
+
+function getThought(){
+    let random = Math.floor(Math.random()*4)
+    txt.innerHTML = `${thought[random]}`
+}
+
+function animate() {
+   
+}
+function toogle (){
+      screen1.classList.toggle('hide')  
+      screen2.classList.toggle('hide')  
+}
